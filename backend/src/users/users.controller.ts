1403e07a089e27ac82576d8d271bd9cf5f5f9dd1
warley004 <warleyjunio2004@gmail.com>
2025-02-23 20:17:44 -0300

    exclusao de arquivos desnecessarios

--- backend/src/users/users.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('users')
-export class UsersController {
-  @Get()
-  getAllUsers() {
-    return { message: 'Lista de usuários' };
-  }
-}
